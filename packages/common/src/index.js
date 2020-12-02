f82d87d632162049072e86a60ed4d58d0f1bd88c
Igor Zheleznov <igorzheleznov@gmail.com>
2020-12-02 16:48:50 +0300

    rootdir

---
+++ packages/common/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.fromCommon = void 0;
+const fromCommon = () => console.log("Common module connected!");
+exports.fromCommon = fromCommon;
+//# sourceMappingURL=index.js.map
