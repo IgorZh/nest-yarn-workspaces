f4383e3e4595b157fb91cd87e87c1b1d7adca490
Jay Mcdoniel <jaymcd@amazon.com>
2020-12-02 10:03:46 -0800

    chore: fix build process

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { fromCommon } from '@nest-yarn-workspaces/common/src';
+import { fromCommon } from '@nest-yarn-workspaces/common';
 
 @Controller()
 export class AppController {
